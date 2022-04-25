ed6642f686b40432a4b5567d959626cf2729381b
leonmavr <0xleo.git@gmail.com>
2022-04-25 21:04:07 +0300

    Do training without using pickled data

--- train.py
+++ train.py
@@ -1,37 +1,56 @@
 #!/usr/bin/env python3
 
-from sklearn import svm
-import pickle as pkl
-import glob
+### Tip:
+# For more refined but more computationally expensive feature vectors,
+# you can change the `cols` and `rows` parameters in `hog` to higher
+# values, e.g. rows=128, cols=128.
+# Row and column aspect ratio depends on the object you're aiming to
+# detect, so for vehicles it's recommended that they're equal.
+###
+
 import os
 this_script_path = os.path.abspath(__file__)
 this_script_folder = os.path.dirname(this_script_path)
+import sys
+sys.path.append(os.path.join(this_script_folder, '..'))
+from hog import hog
+import pickle as pkl
+from sklearn import svm
 
-X = []
-Y = []
+training_dir = os.path.join(this_script_folder, 'kaggle')
 
-training_path = os.path.join(this_script_folder, 'training_data')
+# SVM training vectors
+X, Y = [], []
+positive_files = os.listdir(os.path.join(training_dir, 'vehicles'))
+negative_files = os.listdir(os.path.join(training_dir, 'non-vehicles'))
+n_pos = len(positive_files)
+n_neg = len(negative_files)
 
-# positive labels
-positive_features = glob.glob(os.path.join(training_path, 'vehicles', '*.pkl'))
-for f in positive_features:
-    f_abs_path = os.path.join(training_path, 'vehicles', f)
-    with open(f_abs_path, 'rb') as f:
-        xi = pkl.load(f)
-    X.append(xi)
-    Y.append(1)
+for i, img_file in enumerate(positive_files):
+    try:
+        f_abs_path = os.path.join(training_dir, 'vehicles', img_file)
+        feature_vec = hog(f_abs_path, rows=64, cols=64, mode = 'grey')
+        X.append(feature_vec)
+        Y.append(1)
+        if i % 1000 == 0:
+            print("positive training:", int(100*i/n_pos), "%")
+    except:
+        pass
 
-# negative labels
-negative_features = glob.glob(os.path.join(training_path, 'non-vehicles', '*.pkl'))
-for f in negative_features:
-    f_abs_path = os.path.join(training_path, 'non-vehicles', f)
-    with open(f_abs_path, 'rb') as f:
-        xi = pkl.load(f)
-    X.append(xi)
-    Y.append(0)
+
+for i, img_file in enumerate(negative_files):
+    try:
+        f_abs_path = os.path.join(training_dir, 'non-vehicles', img_file)
+        feature_vec = hog(f_abs_path, rows=64, cols=64, mode = 'grey')
+        X.append(feature_vec)
+        Y.append(0)
+        if i % 1000 == 0:
+            print("negative training:", int(100*i/n_neg), "%")
+    except:
+        pass
 
 clf = svm.SVC()
 clf.fit(X, Y)
-with open('car_classifier.pkl', 'wb') as f:
+with open('car_classifier_kaggle_64x64_grey.pkl', 'wb') as f:
     pkl.dump(clf, f)
-print('SVM training done. Exported the binary classifier as car_classifier.pkl')
+print('SVM training done. Exported the binary classifier as car_classifier*.pkl')
