80d9e8b1edf9f2bef8f855ae5b163059df2a5d16
leonmavr <0xleo.git@gmail.com>
2022-04-27 14:36:00 +0300

    Change some parameters in demo

--- demo.py
+++ demo.py
@@ -10,7 +10,7 @@ this_script_folder = os.path.dirname(this_script_path)
 
 
 def load_classifier(fpath = os.path.join(this_script_folder, 'models',
-                    'car_classifier_64x64.pkl')):
+                    'car_classifier_kaggle_64x64_grey.pkl')):
     with open(fpath, 'rb') as f:
         clf = pickle.load(f)
     return clf
@@ -18,68 +18,74 @@ def load_classifier(fpath = os.path.join(this_script_folder, 'models',
 
 if __name__ == '__main__':
     ### modify this section to change the demo properties
-    # iterate every `coarseness` pixels before attempting to use the classifier
-    coarseness = 8
-    # strip of image starting from the bottom where we do attempt to perform detections
-    horizon = 0.45
+    # strip of image starting from the top where we do attempt to perform detections
+    horizon = 0.6
     assert 0 <= horizon <= 1, "The horizon lies from 0 to 1 relative to the bottom of the frame"
     # pre-trained binary classifier for vehicle detection
     clf = load_classifier()
     # video data
     vid = os.path.join(this_script_folder, 'test_data', 'videos', 'udacity.mp4')
-    # the size of the image patches the HOG create features of
-    patch_size_hog_x = 64
-    patch_size_hog_y = 64
+    # roughly the size of the object in the video
+    vehicle_height = 160
+    vehicle_width = vehicle_height
+    coarseness = int(vehicle_height/2.0)
     # any region with hits (counts) less than this threshold will not be detected
-    heatmap_threshold = 3
+    heatmap_threshold = 0
     bounding_box_color = (50, 255, 0) # as BGR
+    downscale = 4.0
 
     ### do not modify anything below
+    if not 0.99 < downscale < 1.01:
+        vehicle_width = int(vehicle_width / downscale)
+        vehicle_height = int(vehicle_height / downscale)
+        coarseness = int(vehicle_height / 3.0)
     cap = cv2.VideoCapture(vid)
     # Check if the webcam is opened correctly
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
     heatmap = None
 
+    i = 0
     while True:
         valid, frame = cap.read()
+        if not valid:
+            break
         if heatmap is None:
             heatmap = Heatmap((frame.shape[0], frame.shape[1]), heatmap_threshold)
         # the HOG feature detector expects a 2D matrix
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # crop it below the horizon
-        rows, cols = frame.shape
-        #frame = frame[int((1-horizon)*rows):, :]
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rows, cols = frame.shape[0], frame.shape[1]
+        if not 0.99 < downscale < 1.01:
+            # opencv references matrices as (x, y) and numpy as (rows, columns)
+            frame = cv2.resize(frame, (int(cols/downscale), int(rows/downscale)))
+
         horizon = int((1-horizon)*rows)
         # bounding boxes to be refined if they contain a positive detection
         bboxes = []
-        for x in range(0, cols-patch_size_hog_x, coarseness):
-            for y in range(0, rows-patch_size_hog_y, coarseness):
+        for y in range(0, rows-vehicle_height, coarseness):
+            for x in range(0, cols-vehicle_width, coarseness):
                 if y > horizon:
-                    patch_to_detect = frame[y:y+patch_size_hog_y, x:x+patch_size_hog_x]
-                    #print(patch_to_detect)
-                    # TODO: 99 ->1
-                    #if clf.predict([hog(patch_to_detect)])[0] == 1:
-                    if False:
-                        bboxes.append((x, x+patch_size_hog_x, y, y+patch_size_hog_y))
-                        print(bboxes)
-                    #clf.predict([hog(f_abs_path)])[0]
-        bboxes = [(100, 200, 200, 300),
-                  (125, 225, 200, 300),
-                  (125, 225, 200, 300),
-                  (125, 225, 200, 300)]
+                    roi = frame[y:y+vehicle_height, x:x+vehicle_width]
+                    roi = frame[x:x + vehicle_width, y:y+vehicle_height]
+                    if roi.shape[0] != 64 and roi.shape[1] != 64:
+                        roi = cv2.resize(roi, (64, 64))
+                    if clf.predict([hog(roi, rows=64, cols=64)])[0] == 1:
+                        cv2.imwrite('det_%03d.png' % i, roi)
+                        #cv2.rectangle(frame, (x, y), (x+vehicle_width, y+vehicle_height), (0, 255, 0), 3)
+                        i += 1
+                        bboxes.append((x, x+vehicle_width, y, y+vehicle_height))
+                        if len(bboxes) >= 3:
+                            import pdb; pdb.set_trace()
         bboxes = heatmap.refine(bboxes)
+        cv2.imwrite("hm_%03d.jpg" % i, heatmap._heatmap_array)
+
+        heatmap.reset()
         for b in bboxes:
             cv2.rectangle(frame, (b[0], b[1]), (b[2], b[3]), bounding_box_color, 3)
-        """
-                        b = bboxes[i]
-                if len(self._heatmap_array.shape) == 2: # greyscale image
-                    self._heatmap_array = cv2.cvtColor(self._heatmap_array, cv2.COLOR_GRAY2BGR)
-                cv2.rectangle(self._heatmap_array, (b[0], b[1]), (b[2], b[3]),\
-                        color, 3)
-        """
-        cv2.imshow('camera', frame)
+        cv2.imshow('detections', frame)
         c = cv2.waitKey(10)
+        cv2.imwrite('out_%03d.png' % i, frame)
+        i += 1
         if c == ord('q') or not valid:
             break
 
